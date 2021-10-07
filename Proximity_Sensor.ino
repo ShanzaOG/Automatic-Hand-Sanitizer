136861c653e3a42e7009dfba29a72a36c3f17d84
Shanza Allan <allanshanza@gmail.com>
2021-10-07 19:05:30 +0300

    initial commit

---
+++ Proximity_Sensor.ino
@@ -0,0 +1,46 @@
+//Arduino Code
+
+//#include <Servo.h>
+
+#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+Servo myservo; // Pump
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+int pos = 0;
+int pos1 = 90;
+
+void setup() {
+  myservo.attach(7);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  
+}
+void loop() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 1 microseconds
+  digitalWrite(trigPin, HIGH);0
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  if(distance<5){
+    myservo.write(pos);
+    delay(500);
+    myservo.write(pos1);
+    delay(2000);
+    }
+  
+  
+}
